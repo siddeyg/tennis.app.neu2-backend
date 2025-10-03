61ac25a05b6f6afc803b1265cd19b381a924d48b
siddeyg <github@diemachtderworte.de>
2025-10-03 11:09:32 +0200

    Backend: update Schedule, Student models and students routes

--- src/models/Schedule.js
+++ src/models/Schedule.js
@@ -4,6 +4,7 @@ const ScheduleSchema = new mongoose.Schema({
   day: String, // Montag - Samstag
   hour: String, // z.B. "10"
   students: [{ type: mongoose.Schema.Types.ObjectId, ref: "Student" }],
+  coach: { type: mongoose.Schema.Types.ObjectId, ref: "Coach" },
 }); // Referenziert auf Studenten mit dem Modell-Schema "Student"
 
 const Schedule = mongoose.model("Schedule", ScheduleSchema);
--- src/models/Student.js
+++ src/models/Student.js
@@ -8,16 +8,17 @@ const studentSchema = new mongoose.Schema({
   phone: String,
   member: Boolean, // Mitglied ja/nein
   adult: Boolean, //Erwachsenen-Training
-  team: String, // Mannschaft Nein, oder Farbcode
-  trainigGroup: String, // Trainingsgruppe
+  team: String, // Mannschaft
+  groupSize: String, // Gruppengröße (1er, 2er, 3er, 4er)
+  trainigGroup: String, // Trainingsgruppe Kinder: ["Kinderland", "Grün", "Orange", "Gelb Team", "Gelb Hobby"]
   birthDate: String,
-  skillLevel: String, // Trainingsart
+  skillLevel: String, // Erwachsene: ["Anfänger","wenig Fortgeschritten","Fortgeschritten","gute:r Spieler:in"]
   availableTimes: [String], // Liste mit erlaubten Zeiten
   comment: String, // Notiz
-  day: String, //aktuell zugeordnete Tag
-  hour: Number, //aktuell zugeordnete Stunde
+  day: String, //aktuell zugeordnete Tag im Schedule
+  hour: Number, //aktuell zugeordnete Stunde im Schedule
   coach: String,
-  frequence: String,
+  frequence: String, // Trainingshäufigkeit pro Woche (1x, 2x, 3x)
 });
 
 export default mongoose.model("Student", studentSchema);
